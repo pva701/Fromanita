20adf5bdfd7214e24c582108e3667190ca8be7a8
EvgenyZamyatin <jeka_zamyatin@mail.ru>
2015-05-04 21:18:12 +0300

    assets

---
+++ Assets/Scripts/AmanitaController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AmanitaController : MonoBehaviour {
+	private Rigidbody2D rb;
+	public float force = 1.0f;
+	void Awake () {
+		rb = GetComponent<Rigidbody2D> ();
+		rb.AddForce (new Vector2(force, 0));
+	}
+
+
+	void Update () {
+
+	}
+}
